af90b821649f21ed42f7a2ec6e4473f60cf02030
arcanjotiago <tiagoarcanjom@gmail.com>
2024-11-28 14:46:27 -0300

    update all

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -8,15 +8,10 @@ import { userProviders } from './user.providers';
 import { DatabaseModule } from '../database/database.module';
 import { User } from './user.entity';
 
-// import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
-// const moduleMocker = new ModuleMocker(global);
-
-
 describe('Test UserController', () => {
   let userService: UserService;
   let userController: UserController;
   let user: any = new User(); //declaração do User
-  let user2 = 'teste';
 
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -24,22 +19,6 @@ describe('Test UserController', () => {
         controllers: [UserController],
         providers: [UserService, ...userProviders],
     }).compile();
-
-    // .useMocker((token) => {
-    //   const results = ['test1', 'test2'];
-    //     if (token === UserService) {
-    //       return { findAll: jest.fn().mockResolvedValue(results) }; 
-    //     }
-        
-    //     if (typeof token === 'function') {
-    //       const mockMetadata = moduleMocker.getMetadata(
-    //         token,
-    //       ) as MockFunctionMetadata<any, any>;
-    //       const Mock = moduleMocker.generateFromMetadata(mockMetadata);
-    //       return new Mock();
-    //     }
-    // })
-    // .compile();
 
     userController = moduleRef.get(UserController);
     userService = moduleRef.get(UserService);
@@ -61,25 +40,15 @@ describe('Test UserController', () => {
 
   describe('createUser', () => {
     it('This function must add user on database', async () => {
-      
-      //Set das configs do user de teste
-      // user.name = 'Test';
-      // user.email = 'runtest@email.com';
-      // user.password = '000000';
 
-      jest.spyOn(userService, 'createUser').mockImplementation(
-        // () => user
-        () => {
-        // const user: User = new User();
+      jest.spyOn(userService, 'createUser').mockImplementation(() => {
+
         user.name = 'Test';
         user.email = 'runtest@email.com';
         user.password = '000000';
-        // const prt =  await this.userRepository.save(user);
-        // console.log(user);
         return user;
       });
 
-      // data = await userController.createUser(user);
       expect(await userController.createUser(user)).toMatchObject(user);
     });
 
