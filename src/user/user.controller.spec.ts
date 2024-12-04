1d0317ba7ab5df163daea0564c51e9e32307a11b
arcanjotiago <tiagoarcanjom@gmail.com>
2024-12-03 21:18:26 -0300

    update user controller tests

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -5,15 +5,8 @@ import { UserService } from "./user.service";
 import { UserModule } from './user.module';
 import { AuthModule } from '../auth/auth.module';
 import { UserProviders } from './user.providers';
-// jest.mock('./user.providers');
 import { DatabaseModule } from '../database/database.module';
-// jest.mock('../database/database.module');
 import { User } from './user.entity';
-
-// import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
-// const moduleMocker = new ModuleMocker(global);
-
-
 
 
 
@@ -22,9 +15,6 @@ describe('Itegration test user controller', () => {
   let userController: UserController;
   let user:User = new User(); //declaração do User
   let userProviders: UserProviders;
-  // let id:string;
-  // let tokenAuth:string;
-  // let useMockerTest;
 
   
   beforeAll(async () => {
@@ -32,26 +22,11 @@ describe('Itegration test user controller', () => {
         imports: [UserModule, AuthModule, DatabaseModule, ConfigModule.forRoot()],
         controllers: [UserController],
         providers: [UserService, ...UserProviders],
-    })
-    // .useMocker(() => {
-    //   const results = [
-    //     {
-    //       "id": "ac6a5259-6d84-4c1a-97ff-3024ec949986",
-    //       "created_at": "2024-11-29T17:16:34.329Z",
-    //       "name": "seller01",
-    //       "email": "seller01@budget.com",
-    //       "password": "0000000",
-    //       "access_token": "91e40e65-d755-4b9d-a0ed-a6af95fd30b2"
-    //   }
-    //   ];
-    //     return { getUser: jest.fn().mockResolvedValue(results) };
-    // })
-    
+    }) 
     .compile();
 
     userController = await moduleRef.get(UserController);
     userService = await moduleRef.get(UserService);
-    userProviders = await moduleRef.get(UserProviders);
 
   });
 
@@ -83,24 +58,5 @@ describe('Itegration test user controller', () => {
       expect(await userController.createUser(user)).toMatchObject(user);
     });
   });
-  
-
-  // describe('getUserId', () => {
-  //   it('This function must return user from database by id', async () => {
-
-  //     jest.spyOn(userService, 'getUserId').mockImplementation(() => {
-  //       id = '44be2db3-e8fb-4117-b727-068d70302531';
-  //       tokenAuth = 'a4f54d64-55b1-47d4-a665-a289365bf62b'
-  //       user.name = 'Test';
-  //       user.email = 'runtest@email.com';
-  //       user.password = '000000';
-        
-  //       return user;
-  //     });
-
-  //     expect(await userController.getUserId(tokenAuth, id)).toMatchObject(user);
-  //   });
-  // });
-
   
 });
